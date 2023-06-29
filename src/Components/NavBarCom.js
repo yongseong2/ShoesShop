74d876e9b9c4829ccc20862ce1a80c4251be6d0e
yongseong2 <ancjs369@naver.com>
2023-06-29 14:19:47 +0900

    react-query

--- src/Components/NavBarCom.js
+++ src/Components/NavBarCom.js
@@ -3,11 +3,27 @@ import { GiShoppingCart } from 'react-icons/gi'
 import { Link, useNavigate } from 'react-router-dom'
 import RecentShoe from './RecentShoe'
 import { useState } from 'react'
+// import axios from 'axios'
+import { useQuery } from 'react-query'
+import axios from 'axios'
 
 
 function NavBarCom() {
   const navigate = useNavigate()
   const [showRecent, setShowRecent] = useState(false)
+
+  // const result = axios.get('https://codingapple1.github.io/userdata.json')
+  // .then((res)=>{
+  //   return res.data
+  // })
+
+  // 실시간 데이터 공유 가능
+  const result = useQuery('작명', ()=>{
+    return axios.get('https://codingapple1.github.io/userdata.json')
+    .then((res)=>{
+      return res.data
+    })
+  }, { staleTime : 2000 }) // staleTime : 타이머 기능
 
   return (
     <>
@@ -26,6 +42,7 @@ function NavBarCom() {
             <Link to="/cart" className='nav-link'>
               장바구니 <GiShoppingCart size="25" />
             </Link>
+            <NavLink>{result.isLoading? <>로딩중 </>: result.data.name}</NavLink>
           </Nav>
         </Container>
       </Navbar>
--- src/index.js
+++ src/index.js
@@ -6,16 +6,21 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './../src/store/store';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
